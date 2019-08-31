8f9f81819262e438c2aad8b6685ff107702cdf3a
Pierre Grimaud <grimaud.pierre@gmail.com>
2019-08-31 15:05:18 +0200

    Update action

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,9 @@
 ls -la
 pwd
 
+echo $SHOPIFY_STORE_URL
+echo $SHOPIFY_THEME_ID
+echo $THEME_PATH
+
 theme configure --password=$SHOPIFY_PASSWORD --store=$SHOPIFY_STORE_URL --themeid=$SHOPIFY_THEME_ID --dir=$THEME_PATH $*
 theme deploy
