1e8159b4d8852f804cc7ea073fff137223c77ad2
Pierre Grimaud <grimaud.pierre@gmail.com>
2019-08-31 16:02:04 +0200

    Update action

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
 
-ls -la /github/workspace/theme
-
-theme version
-
 theme configure -v --password=$SHOPIFY_PASSWORD --store=$SHOPIFY_STORE_URL --themeid=$SHOPIFY_THEME_ID --dir=$THEME_PATH $ARGS
 theme deploy
