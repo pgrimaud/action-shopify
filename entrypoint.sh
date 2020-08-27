4271983da9e7c551e36a60af54029444b5c06c1f
dav-prog <david.woegerb@gmail.com>
2020-08-27 11:54:58 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
 theme download config/settings_data.json --password=$SHOPIFY_PASSWORD --store=$SHOPIFY_STORE_URL --themeid=$SHOPIFY_THEME_ID --dir=$THEME_PATH $INPUT_ARGS
+
+git add .
+
+git commit -m "Autoupdated settings_data.json"
+
+git push origin/feature/ES-87-update-settings-data-json
